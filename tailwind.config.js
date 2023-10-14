13ce4e9a9432add7fe4ae47df5a7dbe0daaa684c
James Loh <lohjames16@gmail.com>
2023-10-14 16:14:38 -0500

    Fixed daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
