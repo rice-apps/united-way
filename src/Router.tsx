bd9c1f8d2d2ea83dfd231af0a3cac852ad58d218
Nomin <ng53@rice.edu>
2023-10-14 14:22:53 -0500

    init impact-cards

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.tsx'
 import Donation from './pages/Donation/Donation.tsx'
 import NotFound from './pages/NotFound/NotFound.tsx';
-
+import ImpactCards from './pages/impact-cards/impact-cards.tsx';
 
 function Router() {
     return (
@@ -10,7 +10,7 @@ function Router() {
             <Route path="/" element={<Home/>} />
             <Route path="/donation" element={<Donation />} />
             <Route element={<NotFound />} />
-            <Route path="/impact-cards" element={<Donation />} />
+            <Route path="/impact-cards" element={<ImpactCards />} />
       </Routes>
     );
   }
---
+++ src/pages/impact-cards/impact-cards.tsx
@@ -0,0 +1,9 @@
+function ImpactCards() {
+    return (
+      <>
+        <a className=" normal-case text-m">Impact Cards Page</a>
+      </>
+    );
+  }
+  
+  export default ImpactCards;
