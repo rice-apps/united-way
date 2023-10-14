b7e0094c59f97ecd76c30b0232c110b5c1fb7a85
sarahmotteler <sarahmotteler2021@gmail.com>
2023-10-14 14:18:44 -0500

    added sample modal

--- src/pages/ModalTest/ModalTest.tsx
+++ src/pages/ModalTest/ModalTest.tsx
@@ -1,8 +1,20 @@
+
+// button is yelling at me saying that the element might be null and show modal
+// doesn't exist, but it runs fine
 function ModalTest() {
         return (
-          <>
-            <a className=" normal-case text-m">ModalTest :D</a>
-          </>
+                <div className="App">
+                        <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>open modal</button>
+                                <dialog id="my_modal_1" className="modal">
+                                <div className="modal-box">
+                                <h3 className="font-bold text-lg">Hello!</h3>
+                                <p className="py-4">Press ESC key or click outside to close</p>
+                                </div>
+                        <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                        </form>
+                        </dialog>
+                </div>
         );
       }
       
