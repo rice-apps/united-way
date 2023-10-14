65ab23550bc0b687022fde04eea24cb455e9a83d
sarahmotteler <sarahmotteler2021@gmail.com>
2023-10-14 13:43:07 -0500

    created modal test page

---
+++ src/pages/ModalTest/ModalTest.tsx
@@ -0,0 +1,9 @@
+function ModalTest() {
+        return (
+          <>
+            <a className=" normal-case text-m">ModalTest :D</a>
+          </>
+        );
+      }
+      
+      export default ModalTest;
