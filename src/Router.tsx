733e35f51e253133d69f3a5eba13fb3f889b1e84
KataTech <kmh9@rice.edu>
2023-10-14 13:26:57 -0500

    Cannot import aboutUs into routes; debug needed

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'; 
 import Home from './pages/Home/Home.tsx'
 import Donation from './pages/Donation/Donation.tsx'
+import AboutUs from './pages/AboutUs/AboutUs.tsx'
 import NotFound from './pages/NotFound/NotFound.tsx';
 
 
@@ -9,6 +10,7 @@ function Router() {
       <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/donation" element={<Donation />} />
+            <Route path="/aboutus" element={<AboutUs/>}/>
             <Route element={<NotFound />} />
       </Routes>
     );
---
+++ src/pages/AbousUs/AbousUs.jsx
@@ -0,0 +1,9 @@
+function AboutUs() {
+    return (
+      <>
+        <a className=" normal-case text-m">About Us Page</a>
+      </>
+    );
+  }
+  
+  export default AboutUs;
