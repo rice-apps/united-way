4a9e406630d9725e8648aa80019d47f4861c04d4
sarahmotteler <sarahmotteler2021@gmail.com>
2023-10-21 14:07:11 -0500

    added additional modal test function - shows results of an organization's donations

--- src/pages/ModalTest/ModalTest.tsx
+++ src/pages/ModalTest/ModalTest.tsx
@@ -20,5 +20,34 @@ function ModalTest() {
                 </div>
         );
       }
+
+function ResultsModal() {
+        return (
+                <div className="App">
+                        <button className="btn" onClick={()=>document.getElementById('my_modal_1')!.showModal()}>You make it all possible!</button>
+                                <dialog id="my_modal_1" className="modal">
+                                <div className="modal-box">
+                                <h3 className="font-bold text-lg">Here's what the *insert company* United Way campaign made possible last year.</h3>
+                                <ul>
+                                        <li>302 families got support to achieve financial stability</li>
+                                        <li>494 young people helped to succeed in school and in life</li>
+                                        <li>1,682 people recieved physical and behavioral health care support</li>
+                                        <li>134 people got support to escape violent situations</li>
+                                        <li>634 people got support with basic needs like food, housing, and transportation</li>
+                                </ul>
+                                </div>
+                        <form method="dialog" className="modal-backdrop">
+                        <button>close</button>
+                        </form>
+                        </dialog>
+                </div>
+        );
+}
       
-      export default ModalTest;
+const modalFuncs = {
+        ResultsModal,
+        ModalTest
+      }
+      
+      export default modalFuncs;
+        //     export default ResultsModal
