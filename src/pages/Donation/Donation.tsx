628b8f01b0ccb16f33dc98bb563be9952342744f
joy <jy92@rice.edu>
2023-10-28 15:53:42 -0500

    basic buttons

--- src/pages/Donation/Donation.tsx
+++ src/pages/Donation/Donation.tsx
@@ -1,7 +1,31 @@
 function Donation() {
+  const goHome = () => {
+    window.location.href = '/';
+  };
+
+  const shareResults = () => {
+    window.location.href = '/';
+  };
+
+  const company = () => {
+  }
+
     return (
       <>
         <a className=" normal-case text-m">Donation Page</a>
+        <div></div>
+        <button className="btn" onClick={goHome}>GO HOME</button>
+        <div></div>
+
+        Here's what the /company/ United Way campaign* made possible last year:
+        <div>
+          *based on campaign results of /enter dollar value/
+        </div>
+
+        <div></div>
+
+        <button className="btn" onClick={shareResults}>SHARE YOUR RESULTS</button>
+
       </>
     );
   }
