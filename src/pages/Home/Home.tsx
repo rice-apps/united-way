f153737b31bb0e4660f78704e9036eb84592aa3e
jscyyl <jl263@rice.edu>
2023-10-28 14:20:42 -0500

    homepage v1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,29 @@
-//import React, { useState } from 'react';
-function Home() {
+import React, { useState } from 'react';
+const Home: React.FC = () => {
+  const [inputText, setInputText] = useState('');
+  const [outputMap, setOutputMap] = useState<Record<string, number>>({});
+  
+  // prevents non-numbers to be entered
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // Allow numeric digits (0-9) and some special keys (e.g., Backspace, Arrow keys, etc.)
+    const allowedKeys = /[0-9\b]/;
 
+    if (!allowedKeys.test(event.key)) {
+      event.preventDefault();
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newText = event.target.value;
+    setInputText(newText);
+
+    // Call the function that returns the output
+    const processed = multiplyValues(proportions, +newText);
+    setOutputMap(processed);
+  };
+
+
+  // processing function that takes in hard-coded map and the input text and returns the result 
   const multiplyValues = (inputMap: Record<string, number>, multiplier: number): Record<string, number> => {
     const resultHashMap: Record<string, number> = {};
   
@@ -12,14 +35,14 @@ function Home() {
   
     return resultHashMap;
   };
-  
-  const MyComponent: React.FC = () => {
-    // dummy proportions
-    const proportions: Record<string, number> = {
-      'Financial Stability': 0.3,
-      'Early Childhood': 0.5,
-      'Health Care': 0.2,
-    };
+
+  // dummy proportions used for multiplyValues
+  const proportions: Record<string, number> = {
+    'Financial Stability': 0.3,
+    'Early Childhood': 0.5,
+    'Health Care': 0.2,
+  };
+
   return (
     <>
       <a className=" normal-case text-m">Home Page</a>
@@ -27,7 +50,17 @@ function Home() {
         <label className="label">
           <span className="label-text">Donation Amount</span>
         </label>
-        <input type="text" placeholder="Enter a number" className="input input-bordered w-full max-w-xs" />
+        <input 
+          type="text" 
+          value={inputText}
+          onKeyPress={handleKeyPress}
+          onChange={handleInputChange}
+          placeholder="Enter a number"
+          className="input input-bordered w-full max-w-xs" 
+        />
+        <p>Input Text: {inputText}</p>
+        {/* right now just outputs string rep of map */}
+        <p>Output Map: {JSON.stringify(outputMap)}</p>
         <label className="label">
         </label>
       </div>
@@ -36,7 +69,6 @@ function Home() {
     </>
     
   );
-  }
 }
 
 export default Home;
