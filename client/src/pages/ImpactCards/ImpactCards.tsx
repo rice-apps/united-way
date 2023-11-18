d9200aefcb1e704285e652619ebd2a0f6a256bf4
James Loh <lohjames16@gmail.com>
2023-11-18 13:11:31 -0600

    Rounding data

--- client/src/pages/ImpactCards/ImpactCards.tsx
+++ client/src/pages/ImpactCards/ImpactCards.tsx
@@ -8,7 +8,7 @@ function ImpactCard({ imgURL, numData, unitText, descText }: CardInputs) {
           <img src={imgURL} alt="donation img" className="rounded-xl" />
         </figure>
         <div className="card-body items-center text-center ">
-          <h1 className="card-title text-5xl mb-0">{numData}</h1>
+          <h1 className="card-title text-5xl mb-0">{Math.round(numData)}</h1>
           <h2 className="card-body mb-0">{unitText}</h2>
           <p>{descText}</p>
         </div>
